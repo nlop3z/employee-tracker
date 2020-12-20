14a4d66f8219a153351fd2df3e183cf4aea90096
nlopez <nelly.lopez@utexas.edu>
2020-12-20 13:22:23 -0600

    updated what would you like to do section

--- server.js
+++ server.js
@@ -23,8 +23,32 @@ const loadMainPrompts = () => {
             message: 'What would you like to do?',
             choices: [
                 {
-                    name: 'View ALL Employees',
+                    name: 'View All Employees',
                     value: 'VIEW_ALL_EMPLOYEES'
+                },
+                {
+                    name: 'View All Employees By Department',
+                    value: 'VIEW_ALL_EMPLOYEES_DEPARTMENT'
+                },
+                {
+                    name: 'View All Employees By Manager',
+                    value: 'VIEW_ALL_EMPLOYEES_MANAGER'
+                },
+                {
+                    name: 'Add Employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Remove Employee',
+                    value: 'REMOVE_EMPLOYEE'
+                },
+                {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                },
+                {
+                    name: 'Update Employee Manager',
+                    value: 'UPDATE_EMPLOYEE_MANAGER'
                 },
                 {
                     name: 'Quit',
